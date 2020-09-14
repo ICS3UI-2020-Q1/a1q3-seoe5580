a378550f9e65135b023a810c290db702337c8148
seoe5580 <seoe5580@wrdsb.ca>
2020-09-14 20:27:06 +0000

    Finished the money assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Allows you to see the total amount of money that was spent for school supplies
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +11,44 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creat a Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // Asks the user how many chromebook chargers they want
+    System.out.println("How many chromebook chargers?");
+
+    // Gets users input for the amount of chargers they want
+    int charger = input.nextInt();
+
+    // Asks the user how many motherboards they want
+    System.out.println("How many motherboards?");
+
+    // Gets users input for the amount of motherboards they want
+    int motherboard = input.nextInt ();
+
+    // Asks the user how many mice they want
+    System.out.println("How many mice?");
+
+    // Gets users input for the amount of mice they want
+    int mice = input.nextInt ();
+
+    // Calculates how much everything is without tax
+    double subtotal = charger * 34.99 + motherboard * 127.50 + mice * 18.00;
+
+    // Shows the subtotal
+    System.out.println("Subtotal = $" + subtotal);
+
+    // Calculates the taxes of the subtotal
+    double taxes = 0.13 * subtotal;
+
+    //shows the users the tax of your subtotal
+    System.out.println("Taxes = $" + taxes);
+
+    // Calculates the total
+    double total = subtotal + taxes;
+
+    // Shows the users the final amount
+    System.out.println("Total = $" + total);
+
   }
 }
